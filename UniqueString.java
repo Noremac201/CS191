028d0bdd59947374ed5666e56d3f7efdd19c7616
Cameron Moberg <cjmoberg@gmail.com>
2016-01-12 16:57:39 -0600

    Update UniqueString.java

--- UniqueString.java
+++ UniqueString.java
@@ -2,26 +2,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UniqStr {
-	public static void main(String[] args) {
-		String str_test = args[0];
-		int end = 0;
-		int checker = 0;
 
+	public static Integer checkString(String str_test) {
 		char[] char_array = str_test.toCharArray();
 		Set<Character> char_hash = new HashSet<Character>();
 		for (char ch : char_array) {
 			if (char_hash.contains(ch)) {
-				end = -1;
-				checker = -1;
-				break;
+				return 1;
 			} else {
 				char_hash.add(ch);
 			}
 		}
-		if (end != -1 && checker == -1) {
-			System.out.println("String is not unique");
-		} else {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		if (checkString(args[0]) == 0) {
 			System.out.println("String is unique");
+		} else {
+			System.out.println("String is not unique");
 		}
+
 	}
 }
